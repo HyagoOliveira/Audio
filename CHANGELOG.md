66e201933a45e641a56c7e257772b9e5b259daae
Hyago Oliveira <hyagogow@gmail.com>
2023-03-07 18:39:37 -0300

    Add AudioSourceAudioGroup component

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -12,6 +12,7 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 - SliderAudioGroup component
 - TextMeshProAudioGroup component
 - ToggleAudioGroup component
+- AudioSourceAudioGroup component
 
 ### Changed
 - Remove DisallowMultipleComponent attribute from AudioSourceDictionary
---
+++ Runtime/Groups/UI/AudioSourceAudioGroup.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+namespace ActionCode.Audio
+{
+    /// <summary>
+    /// Plays an <see cref="AudioSource"/> when the <see cref="AudioGroup.Volume"/> changes.
+    /// </summary>
+    [DisallowMultipleComponent]
+    [RequireComponent(typeof(AudioSource))]
+    public sealed class AudioSourceAudioGroup : AbstractUIAudioGroup
+    {
+        [SerializeField] private AudioSource audioSource;
+
+        protected override void Reset()
+        {
+            base.Reset();
+            audioSource = GetComponent<AudioSource>();
+        }
+
+        private void OnEnable() => audioGroup.OnVolumeChanged += HandleVolumeChanged;
+        private void OnDisable() => audioGroup.OnVolumeChanged -= HandleVolumeChanged;
+
+        protected override void SetInitialValue() { }
+
+        private void HandleVolumeChanged(float volume)
+        {
+            var hasVolume = !Mathf.Approximately(volume, 0f);
+            var canPlay = hasVolume && !audioSource.isPlaying;
+            if (canPlay) audioSource.Play();
+        }
+    }
+}
