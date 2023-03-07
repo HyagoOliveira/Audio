da7f42ece575e9e77544829a091117a13ca9d792
Hyago Oliveira <hyagogow@gmail.com>
2023-03-07 18:54:38 -0300

    Rename class AudioManagerData -> AudioData

--- Runtime/Manager/AudioManager.cs
+++ Runtime/Manager/AudioManager.cs
@@ -18,13 +18,13 @@ namespace ActionCode.Audio
         [SerializeField, Tooltip("The Voice Effects Group inside the AudioMixer asset.")]
         private AudioGroup voiceEffectsGroup;
 
-        private void Awake() => Load(new AudioManagerData());
+        private void Awake() => Load(new AudioData());
 
         /// <summary>
         /// Loads all groups using the given data.
         /// </summary>
         /// <param name="data">The audio data to load.</param>
-        public void Load(AudioManagerData data)
+        public void Load(AudioData data)
         {
             if (backgroundGroup) backgroundGroup.Volume = data.BackgroundVolume;
             if (soundEffectsGroup) soundEffectsGroup.Volume = data.SoundEffectsVolume;
@@ -36,7 +36,7 @@ namespace ActionCode.Audio
         /// Saves all groups into the given data.
         /// </summary>
         /// <param name="data">The audio data to save.</param>
-        public void Save(ref AudioManagerData data)
+        public void Save(ref AudioData data)
         {
             if (backgroundGroup) data.BackgroundVolume = backgroundGroup.Volume;
             if (soundEffectsGroup) data.SoundEffectsVolume = soundEffectsGroup.Volume;
--- Runtime/Serialization/AudioData.cs
+++ Runtime/Serialization/AudioData.cs
@@ -6,7 +6,7 @@ namespace ActionCode.Audio
     /// Data class for <see cref="AudioManager"/>.
     /// </summary>
     [Serializable]
-    public sealed class AudioManagerData
+    public sealed class AudioData
     {
         public float BackgroundVolume { get; set; } = 1F;
         public float SoundEffectsVolume { get; set; } = 1F;
