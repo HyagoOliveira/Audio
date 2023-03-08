75790673f55a4c81020db89b05f1f487ff7fabe8
Hyago Oliveira <hyagogow@gmail.com>
2023-03-08 10:56:48 -0300

    Add AudioSourceExtension

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -13,6 +13,7 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 - TextMeshProAudioGroup component
 - ToggleAudioGroup component
 - AudioSourceAudioGroup component
+- AudioSourceExtension
 
 ### Changed
 - Remove DisallowMultipleComponent attribute from AudioSourceDictionary
---
+++ Runtime/Extensions/AudioSourceExtension.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace ActionCode.Audio
+{
+    /// <summary>
+    /// Extension class for <see cref="AudioSource"/>.
+    /// </summary>
+    public static class AudioSourceExtension
+    {
+        /// <summary>
+        /// Sets the Audio source to play as a 2D sound.
+        /// </summary>
+        /// <param name="source"></param>
+        public static void SetSpatialBlendTo2D(this AudioSource source) => source.spatialBlend = 0F;
+
+        /// <summary>
+        /// Sets the Audio source to play as a 3D sound.
+        /// </summary>
+        /// <param name="source"></param>
+        public static void SetSpatialBlendTo3D(this AudioSource source) => source.spatialBlend = 1F;
+    }
+}
