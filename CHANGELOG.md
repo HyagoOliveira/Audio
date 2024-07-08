19fc88c1a2bee0a02f102e51586532c36fdd04ef
Hyago Oliveira <hyagogow@gmail.com>
2024-07-08 12:23:25 -0300

    Add AudioDisabler

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,7 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 ## [Unreleased]
 ### Added
 - AudioLooper
+- AudioDisabler
 
 ## [3.0.1] - 2023-03-25
 
---
+++ Runtime/Gameplay/AudioDisabler.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace ActionCode.Audio
+{
+    /// <summary>
+    /// Disables this GameObject after the audio clip ends.
+    /// </summary>
+    [DisallowMultipleComponent]
+    [RequireComponent(typeof(AudioSource))]
+    public sealed class AudioDisabler : MonoBehaviour
+    {
+#pragma warning disable CS0108 // Member hides inherited member; missing new keyword
+        [SerializeField] private AudioSource audio;
+#pragma warning restore CS0108 // Member hides inherited member; missing new keyword
+
+        private void Reset() => audio = GetComponent<AudioSource>();
+        private void OnEnable() => Invoke(nameof(Disable), audio.clip.length);
+        private void OnDisable() => CancelInvoke();
+
+        private void Disable() => gameObject.SetActive(false);
+    }
+}
