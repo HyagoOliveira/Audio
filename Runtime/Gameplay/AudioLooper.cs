90da0bac4661d9da22429bad840775e081673131
Hyago Oliveira <hyagogow@gmail.com>
2024-07-08 12:02:28 -0300

    Add AudioLooper

---
+++ Runtime/Gameplay/AudioLooper.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+namespace ActionCode.Audio
+{
+    /// <summary>
+    /// Plays the AudioSource clip in loop after played the start sound.
+    /// </summary>
+    [DisallowMultipleComponent]
+    [RequireComponent(typeof(AudioSource))]
+    public sealed class AudioLooper : MonoBehaviour
+    {
+#pragma warning disable CS0108 // Member hides inherited member; missing new keyword
+        [SerializeField] private AudioSource audio;
+#pragma warning restore CS0108 // Member hides inherited member; missing new keyword
+        [SerializeField] private AudioClip startSound;
+
+        private void Reset()
+        {
+            audio = GetComponent<AudioSource>();
+            audio.loop = true;
+            audio.playOnAwake = false;
+        }
+
+        private void OnEnable()
+        {
+            var time = startSound.length;
+
+            audio.PlayOneShot(startSound);
+            Invoke(nameof(PlayLoopSound), time);
+        }
+
+        private void OnDisable() => CancelInvoke();
+
+        private void PlayLoopSound() => audio.Play();
+    }
+}
